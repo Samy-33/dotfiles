b73273e7fb1de453a762bb9462362d548bf34126
Saket Patel <saket.is.sam@gmail.com>
2024-08-03 15:56:14 +0530

    cphelper+more snippets

--- dotcom/.config/nvim/lua/user/config/cphelper.lua
+++ dotcom/.config/nvim/lua/user/config/cphelper.lua
@@ -2,4 +2,6 @@
 vim.g["cph#dir"] = vim.fn.expand("$HOME/Documents/cp/cphelper")
 vim.g["cph#lang"] = "cpp"
 vim.g["cph#cpp#compile_command"] = "g++ -g solution.cpp -o cpp.out"
-return nil
+vim.keymap.set({"n"}, "<localleader>cpr", ":CphReceive<cr>", {desc = "Fetch problem", silent = false})
+vim.keymap.set({"n"}, "<localleader>cpt", ":CphTest<cr>", {desc = "Compile and Test", silent = false})
+return vim.keymap.set({"n"}, "<localleader>cptt", ":CphRetest<cr>", {desc = "Retest", silent = false})
