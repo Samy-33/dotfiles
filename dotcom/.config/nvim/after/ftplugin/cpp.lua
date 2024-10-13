7a1ddc94727fb2bbf19c7807b1f7bb9efb959add
Saket <saket.is.sam@gmail.com>
2024-10-13 23:46:52 +0530

    Compile all fennel files

--- dotcom/.config/nvim/after/ftplugin/cpp.lua
+++ dotcom/.config/nvim/after/ftplugin/cpp.lua
@@ -1,4 +1,4 @@
--- [nfnl] Compiled from ftplugin/cpp.fnl by https://github.com/Olical/nfnl, do not edit.
+-- [nfnl] Compiled from after/ftplugin/cpp.fnl by https://github.com/Olical/nfnl, do not edit.
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
