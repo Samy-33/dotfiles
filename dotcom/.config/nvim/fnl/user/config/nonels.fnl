a3f80ac36af30630739d7370f00354ff5c1b2619
Saket <saket.is.sam@gmail.com>
2024-12-16 11:41:01 +0530

    Updates clojure form defs for paredit

--- dotcom/.config/nvim/fnl/user/config/nonels.fnl
+++ dotcom/.config/nvim/fnl/user/config/nonels.fnl
@@ -2,3 +2,5 @@
 (local null-ls (autoload :null-ls))
 
 (null-ls.setup {:sources [null-ls.builtins.formatting.fnlfmt]})
+
+{}
--- dotcom/.config/nvim/lua/user/config/nonels.lua
+++ dotcom/.config/nvim/lua/user/config/nonels.lua
@@ -2,4 +2,5 @@
 local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
 local null_ls = autoload("null-ls")
-return null_ls.setup({sources = {null_ls.builtins.formatting.fnlfmt}})
+null_ls.setup({sources = {null_ls.builtins.formatting.fnlfmt}})
+return {}
