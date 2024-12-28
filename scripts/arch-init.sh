fc48db861624dc75604c9440acbfa663c6fcb82d
Saket Patel <saket.is.sam@gmail.com>
2024-12-28 11:47:29 +0530

    feat: uses ghostty instead of kitty

--- scripts/arch-init.sh
+++ scripts/arch-init.sh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
-sudo pacman -S stow 
-
 stow --target=$HOME dotcom
 stow --target=$HOME dotarch
 
 # Removes the stowed symlinks
-remove-stow () {
+clean () {
   stow --target=$HOME -D dotcom
   stow --target=$HOME -D dotarch
 }
