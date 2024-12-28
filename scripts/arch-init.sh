f6f86efe2717f4c0e06e1cb711b840039521b742
Saket Patel <saket.is.sam@gmail.com>
2024-12-28 12:35:11 +0530

    adds better tmux config

--- scripts/arch-init.sh
+++ scripts/arch-init.sh
@@ -4,7 +4,7 @@ stow --target=$HOME dotcom
 stow --target=$HOME dotarch
 
 # Removes the stowed symlinks
-clean () {
+stow_clean () {
   stow --target=$HOME -D dotcom
   stow --target=$HOME -D dotarch
 }
